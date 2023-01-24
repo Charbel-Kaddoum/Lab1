2aeda9c0470f660c95548d352d47123fbc075d2e
Charbel-Kaddoum <charbelkadd@gmail.com>
2023-01-24 20:55:03 +0200

    year added

--- Me.java
+++ Me.java
@@ -3,5 +3,6 @@ public static void main(String[] args){
 System.out.println("Charbel Kaddoum");
 		System.out.println(18);
 		System.out.println("Electrical Engineering");
+		System.out.println("Year 2022");
 	}
 }
